c809100e5180d3804a7f9830b7220fbf94467c80
Laura Jodz <laurajodz@gmail.com>
2017-12-30 15:22:12 -0800

    updated client.js arrays

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,31 +1,25 @@
 const shoppingList = [
-  name: "pasta sauce",
-  name: "Parmesean cheese",
-  name: "quesadilla shells",
-  name: "sausage",
-  name: "pasta",
-  name: "onion",
-  name: "eggs",
-  name: "cabbage",
-  name: "ground turkey",
-  name: "ground cumin"
+  {name: "pasta", unit: "pounds", amount: 1},
+  {name: "Parmesean cheese", unit: "cups", amount: 1},
+  {name: "mozzarella cheese", unit: "cups", amount: 1},
+  {name: "pasta sauce", unit: "cups", amount: 2},
+  {name: "quesadillas", unit: "shells", amount: 3},
+  {name: "cheddar cheese", unit: "ounces", amount: 5},
+  {name: "sausage", unit: "ounces", amount: 14},
+  {name: "onion", unit: "cups", amount: 2},
+  {name: "cabbage", unit: "cups", amount: 6},
+  {name: "ground turkey", unit: "pounds", amount: 2},
+  {name: "ground cumin", unit: "teaspoons", amount: 2},
+  {name: "eggs", unit: "whole", amount: 1}
 ];
 
 const recipes = [
-  {
-  name: "Baked ziti",
-  ingredients: ["1 pound pasta", "1 cup Parmesean cheese", "1 cup Mozzarella cheese", "2 cups pasta sauce"]
-  },
-  {
-  name: "Quesadillas",
-  ingredients: ["3 quesadilla shells", "5 oz cheddar cheese"]
-  },
-  {
-  name: "Sausage and cabbage",
-  ingredients: ["14 oz sausage", "1 onion", "6 cups cabbage"]
-  },
-  {
-  name: "Turkey chili",
-  ingredients: ["2 pounds ground turkey", "1 onion", "2 tsp ground cumin"]
-  },
+  {name: "Baked ziti",
+  ingredients: ["1 pound pasta", "1 cup Parmesean cheese", "1 cup mozzarella cheese", "2 cups pasta sauce"]},
+  {name: "Quesadillas",
+  ingredients: ["3 quesadilla shells", "5 oz cheddar cheese"]},
+  {name: "Sausage and cabbage",
+  ingredients: ["14 oz sausage", "1 cup onion", "6 cups cabbage"]},
+  {name: "Turkey chili",
+  ingredients: ["2 pounds ground turkey", "1 cup onion", "2 tsp ground cumin"]},
 ];
