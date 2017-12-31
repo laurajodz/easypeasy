b68cf0b699bfeebc6f2ed16a851df6e37aef5887
Laura Jodz <laurajodz@gmail.com>
2017-12-31 10:46:16 -0800

    added js for mealPlan and shoppingList pages

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,17 +1,18 @@
 const mockShoppingList = {
   "shoppingList": [
-    {name: "pasta", unit: "pounds", amount: 1},
-    {name: "Parmesean cheese", unit: "cups", amount: 1},
-    {name: "mozzarella cheese", unit: "cups", amount: 1},
-    {name: "pasta sauce", unit: "cups", amount: 2},
-    {name: "quesadillas", unit: "shells", amount: 3},
-    {name: "cheddar cheese", unit: "ounces", amount: 5},
-    {name: "sausage", unit: "ounces", amount: 14},
-    {name: "onion", unit: "cups", amount: 2},
-    {name: "cabbage", unit: "cups", amount: 6},
-    {name: "ground turkey", unit: "pounds", amount: 2},
-    {name: "ground cumin", unit: "teaspoons", amount: 2},
-    {name: "eggs", unit: "whole", amount: 1}
+    {name: "pasta", unit: "pound(s)", amount: 1, section: "ethnic", recipe: "Baked ziti"},
+    {name: "Parmesean cheese", unit: "cup(s)", amount: 1, section: "deli & cheese", recipe: "Baked ziti"},
+    {name: "mozzarella cheese", unit: "cup(s)", amount: 1, section: "deli & cheese", recipe: "Baked ziti"},
+    {name: "pasta sauce", unit: "cup(s)", amount: 2, section: "ethnic", recipe: "Baked ziti"},
+    {name: "quesadillas", unit: "shell(s)", amount: 3, section: "bakery & bread", recipe: "Quesadillas"},
+    {name: "cheddar cheese", unit: "ounce(s)", amount: 5, section: "deli & cheese", recipe: "Quesadillas"},
+    {name: "sausage", unit: "ounce(s)", amount: 14, section: "meat", recipe: "Sausage & cabbage"},
+    {name: "cabbage", unit: "cup(s)", amount: 6, section: "produce", recipe: "Sausage & cabbage"},
+    {name: "onion", unit: "cup(s)", amount: 1, section: "produce", recipe: "Sausage & cabbage"},
+    {name: "onion", unit: "cup(s)", amount: 1, section: "produce", recipe: "Turkey chili"},
+    {name: "ground turkey", unit: "pound(s)", amount: 2, section: "meat", recipe: "Turkey chili"},
+    {name: "ground cumin", unit: "teaspoon(s)", amount: 2, section: "spices", recipe: "Turkey chili"},
+    {name: "eggs", unit: "whole", amount: 1, section: "dairy & eggs", recipe: "none"}
   ]
 };
 
@@ -27,7 +28,7 @@ const mockRecipes = {
     instructions: "Lorem ipsum",
     recipeId: 000002},
     {name: "Sausage and cabbage",
-    ingredients: ["14 oz sausage", "1 cup onion", "6 cups cabbage"],
+    ingredients: ["14 oz sausage", "6 cups cabbage", "1 cup onion"],
     instructions: "Lorem ipsum",
     recipeId: 000003},
     {name: "Turkey chili",
@@ -41,9 +42,11 @@ const mockMealPlan = {
   "mealPlan": [
     {name: "Week of Jan 1 2018",
     recipeIds: [000001, 000003, 000004],
+    recipeNames: ["Baked ziti", "Sausage & cabbage", "Turkey chili"],
     mealPlanID: 010118}
   ]
 };
+
 
 function getRecipes(callbackFn) {
   setTimeout(function(){ callbackFn(mockRecipes)}, 1);
@@ -51,7 +54,7 @@ function getRecipes(callbackFn) {
 
 function displayRecipes(data) {
   for (index in data.recipes) {
-    $('body').append(
+    $('.recipes').append(
       '<input id="recipe-checkbox" type="checkbox">' +
       '<p>' + data.recipes[index].name + '</p>' +
       '<img src="' + data.recipes[index].photo + '">');
@@ -59,16 +62,64 @@ function displayRecipes(data) {
 };
 
 
+function getShoppingList(callbackFn) {
+  setTimeout(function(){ callbackFn(mockShoppingList)}, 1);
+};
+
+function displayShoppingList(data) {
+  for (index in data.shoppingList) {
+    $('.shopping-list').append(
+      '<input id="shopping-item-checkbox" type="checkbox">' +
+      '<p>' + data.shoppingList[index].name + '</p>' +
+      '<p>' + data.shoppingList[index].amount + ' ' + data.shoppingList[index].unit + '</p>' +
+      '<button class="edit-shopping-list">edit</button>');
+  }
+};
+
+function addToShoppingList(itemName) {
+  mockShoppingList.shoppingList.push({name: itemName});
+}
+
+function handleAddToShoppingList() {
+  $('#shopping-list-add').submit(function(event) {
+    event.preventDefault();
+    const newItemName = $('.shopping-list-entry').val();
+    $('.shopping-list-entry').val('');
+    addToShoppingList(newItemName);
+    console.log(newItemName);
+  });
+}
+
+
+function getMealPlan(callbackFn) {
+  setTimeout(function(){ callbackFn(mockMealPlan)}, 1);
+};
+
+function displayMealPlan(data) {
+  for (index in data.mealPlan) {
+    $('.meal-plan').append(
+      '<h2>' + data.mealPlan[index].name + '</h2>' +
+      '<h3>' + data.mealPlan[index].recipeNames + '</h3>');
+  }
+};
+
+
 $(function() {
   getRecipes(displayRecipes);
 
-  $('#recipe-checkbox').change(function() {
+  getShoppingList(displayShoppingList);
+
+  handleAddToShoppingList();
+
+  getMealPlan(displayMealPlan);
+
+  $('#recipe-checkbox').onchange = function() {
     console.log("hello");
     var totalRecipesSelected = $('input[type=checkbox]:checked').length;
     console.log(totalRecipesSelected);
     $('.recipe-counter').html(
       `<h2>Recipes selected: ${totalRecipesSelected}</h2>`
     )
-  });
+  };
 
 });
