063b7cad1810fde2a33ec19482adb14f15f2e83f
Laura Jodz <laurajodz@gmail.com>
2018-01-04 00:46:50 -0800

    connected to recipe API

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,7 @@
-function getRecipeDataFromApi(searchTerm, callback) {
+function getRecipes(searchTerm) {
   const query = {
     q: `${searchTerm}`,
-    to: 50
+    to: 10
   }
   $.ajax({
     url:'https://api.edamam.com/search?app_id=c5e83e4d&app_key=63b608e29d4873fd592f2304be5930d1',
@@ -9,107 +9,19 @@ function getRecipeDataFromApi(searchTerm, callback) {
     data: query,
     dataType:"jsonp"
   }).done(res => {
-    var recipeResults = res.hits;
-    for (var i=0; i<recipeResults.length; i++){
-      var recipeReturn = recipeResults[i].recipe;
-      var recipeReturnName = recipeReturn.label;
-      var recipeReturnImage = recipeReturn.image;
-      var recipeReturnInstructionsUrl = recipeReturn.url;
-      var recipeReturnIngredients = recipeReturn.ingredients;
-      var ingredientList = $('<ul>');
-      for (var j=0; j<recipeReturnIngredients.length; j++) {
-        var returnIngredient = recipeReturnIngredients[j].text;
-        var li = $('<li>').html(returnIngredient);
-        ingredientList.append(li);
-      }
-      var recipe_name = $('<p>').html(recipeReturnName);
-      var recipe_image = $('<img>').attr('src', recipeReturnImage);
-      var label = $('<p>').html('Ingredients');
-      // var recipe_instructions = $('<a href').html('recipeReturnInstructionsUrl');
-      $(".recipesReturned").append(recipe_name, recipe_image, label, ingredientList);
-    }
-    if($.isEmptyObject(recipeResults)){
-      $('.noresults').prop('hidden', false);
-    }
+    displayRecipes(res.hits);
   })
 };
 
-
-
-
-function getFoodDataFromApi(searchterm, callback) {
-  const queryFood = {
-    ingr: `${searchterm}`
-  }
-  $.ajax({
-    url:'https://api.edamam.com/api/food-database/parser?app_id=10d53124&app_key=25887721367184b9b11f25937af1f6d',
-    method:'GET',
-    data: queryFood,
-    dataType:"jsonp"
-  }).done(res => {
-})
-};
-
-
-
-function getRecipes() {
-     return new Promise((resolve, reject) => {
-       $.ajax({
-         url: 'mocks/recipes.json',
-         dataType:'json',
-       }).done(data => {
-          resolve(data);
-       }).fail(err => {
-         console.log(err);
-       });
-     });
-  };
-
 function displayRecipes(data) {
-  for (index in data.recipes) {
-    $('.recipes').append(
-      '<input class="recipe-checkbox" name="check" type="checkbox">' +
-      '<p class="recipeName">' + data.recipes[index].name + '</p>' +
-      '<img src="' + data.recipes[index].photo + '" class="recipeImage"></br>');
-  }
-};
-
-
-
-
-function getShoppingList() {
-   return new Promise((resolve, reject) => {
-     $.ajax({
-       url: '../mocks/shoppinglist.json',
-       dataType:'json',
-     }).done(data => {
-        resolve(data);
-     }).fail(err => {
-       console.log(err);
-     });
-   });
-};
-
-function displayShoppingList(data) {
-  $('.shopping-list-items')
+    $('.recipesReturned')
       .append(data.map(item => `<li>
-            <span class="non_edit">${item.name}, ${item.amount} ${item.unit} </span>
-            <span class="edit">
-                <input type="text" value="${item.name}"/>
-                <button>Submit</button>
-            </span>
-            <div class="closebtn">&times;</div>
-            <div class="editbtn">edit</div>
-            </li>`));
-};
-
-
-
-function addToShoppingList(itemName) {
-  $('.added-items')
-    .append('<li>' + itemName + '</li>');
-    // '<input id="shopping-item-checkbox" type="checkbox">' +
-    // '<p>' + itemName + '</p>'
+          <a href="${item.recipe.url}" target="_blank"><img class="resultsimg" src="${item.recipe.image}" alt="${item.recipe.label}"></a></br>
+          <label class="recipeName">${item.recipe.label}<input class="recipe-checkbox" name="check" type="checkbox"></label></li>`))
+    if ($.isEmptyObject(data)) {
+      $('.noresults').prop('hidden', false);
+      $('.recipes').prop('hidden', true);
+    }
 };
 
 
@@ -139,57 +51,129 @@ function displayMealPlan(data) {
 
 
 
+// function getFoodDataFromApi(searchterm) {
+//   const queryFood = {
+//     ingr: `${searchterm}`
+//   }
+//   $.ajax({
+//     url:'https://api.edamam.com/api/food-database/parser?app_id=10d53124&app_key=25887721367184b9b11f25937af1f6d',
+//     method:'GET',
+//     data: queryFood,
+//     dataType:"jsonp"
+//   }).done(res => {
+//     populateDropdown(res);
+//   })
+// };
+
+// function populateDropdown() {
+//   let dropdown = $('#food-dropdown');
+//
+//   dropdown.empty();
+//   dropdown.append('<option selected="true" disabled>Add an item</option>');
+//   dropdown.prop('selectedIndex', 0);
+//
+//   const url = 'https://api.edamam.com/api/food-database/parser?app_id=10d53124&app_key=25887721367184b9b11f25937af1f6d';
+//
+//   // Populate dropdown with list of provinces
+//   $.getJSON(url, function (data) {
+//     $.each(data, function (key, entry) {
+//       dropdown.append($('<option></option>').text(entry.text));
+//     })
+//   });
+// };
+
+// function getShoppingList() {
+//    return new Promise((resolve, reject) => {
+//      $.ajax({
+//        url: '../mocks/shoppinglist.json',
+//        dataType:'json',
+//      }).done(data => {
+//         resolve(data);
+//      }).fail(err => {
+//        console.log(err);
+//      });
+//    });
+// };
+
+function displayShoppingList(data) {
+  $('.shopping-list-items')
+      .append(data.map(item => `<li>
+            <span class="non_edit">${item.name}, ${item.amount} ${item.unit} </span>
+            <span class="edit">
+                <input type="text" value="${item.name}"/>
+                <button>Submit</button>
+            </span>
+            <div class="closebtn">&times;</div>
+            <div class="editbtn">edit</div>
+            </li>`));
+};
+
+function addToShoppingList(itemName) {
+  $('.added-items')
+    .append('<li>' + itemName + '</li>');
+    // '<input id="shopping-item-checkbox" type="checkbox">' +
+    // '<p>' + itemName + '</p>'
+};
+
+
+
+
 $(function() {
   // getRecipes()
   //   .then(data => {
   //     displayRecipes(data);
   //   });
 
-  getShoppingList()
-      .then(data => {
-        displayShoppingList(data);
-      });
+  // getSampleRecipes()
+  //load the recipes page with 5 sample recipes
 
   // getMealPlan()
   //   .then(data => {
   //     displayMealPlan(data);
   //   });
 
+  // getShoppingList()
+  //     .then(data => {
+  //       displayShoppingList(data);
+  //     });
+// populateDropdown();
+
+
+
   //event listener for button click from home page to recipes page
   $('#beginbtn').on('click', function() {
     window.location = 'recipes.html';
   });
 
-  //event handler for counter of recipes selected for meal plan
+  //load sample recipes on recipe page
+
+  //event listener for button click for recipe search
+  $('.recipe-search').submit(event => {
+    $('.recipesSamples').hide();
+    event.preventDefault();
+    const queryText = $('.recipeSearch-entry').val();
+    $('.recipeSearch-entry').val('');
+    getRecipes(queryText);
+  });
+
+  //event listener to select recipes for meal plan and handle counter
   $('.recipes').on('change', '.recipe-checkbox', function(e){
     var totalRecipesSelected = $('input[type=checkbox]:checked').length;
     $('.counter').html(
       `<h2>Recipes selected: ${totalRecipesSelected}</h2>`
     )
-  });
-
-  //event listener for button click for recipe search
-  $('.recipe-search').submit(event => {
-    $('.recipes').hide();
-    event.preventDefault();
-    const queryText = $('.recipeSearch-entry').val();
-    $('.recipeSearch-entry').val('');
-    getRecipeDataFromApi(queryText);
-  });
-
-  //event listener to select recipes for meal plan
-  $('.recipePhoto').on('click', function() {
-      //need something here to capture checked recipes and display their names
-      console.log('hold');
+    var recipeName = $('.recipeName').val();
+    $('recipesSelected').append(
+      `<li>recipeName</li>`
+    )
   });
 
   //event listener for button click from recipe selection page to meal plan summary page
-  $('#addrecipebtn').on('click', function() {
+  $('#submitrecipesbtn').on('click', function() {
     //need something here to bring recipes to meal plan page and ingredients to shopping list page
     window.location = 'mealPlan.html';
   });
 
-  //event listener to delete items on shopping list if recipe is deleted from meal plan
 
 
 
@@ -198,13 +182,22 @@ $(function() {
     window.location = 'shoppingList.html';
   });
 
+  //event listener to go back to recipes page from meal plan page to add more
+
+  //event listener to create new meal plan
+
+  //event listener to delete recipes from meal plan (and its items on shopping list)
+
+
+
+
   //event listener for button click to add an item to shopping list
   $('.additembtn').on('click', function() {
     console.log('hello');
     event.preventDefault();
-    const newItemName = $('.shoppingList-entry').val();
+    const newItemName = $('.shoppingList-entry').val(); //change shoppinglist-entry
     console.log(newItemName);
-    $('.shoppingList-entry').val('');
+    $('.shoppingList-entry').val(''); //change shoppinglist-entry
     addToShoppingList(newItemName);
   });
 
@@ -216,7 +209,7 @@ $(function() {
   }
 }, false);
 
-  // event listener to hideitem from shopping list
+  // event listener to hide item (delete) from shopping list
     $('.closebtn').on('click', function() {
       var close = document.getElementsByClassName("closebtn");
       var i;
